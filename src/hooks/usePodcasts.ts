c129e2629dd9b0e350c164da88de22f2220758e2
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-10-21 14:59:11 +0200

    added a better format for podcast coming from api

--- src/hooks/usePodcasts.ts
+++ src/hooks/usePodcasts.ts
@@ -3,15 +3,14 @@ import { fetchAndCache } from "@/services/fetchAndCache";
 import { PODCAST_NAMING } from "@/types";
 import { useEffect, useMemo, useState } from "react";
 
-type PodcastEntry = {
-  id: { attributes: { "im:id": string } };
-  "im:name": { label: string };
-  "im:artist": { label: string };
-  "im:image": [
-    Array<{
-      label: string;
-    }>
-  ];
+type Podcast = {
+  id: string;
+  title: string;
+  artist: string;
+  images: Array<{
+    attributes: { height: string };
+    label: string;
+  }>;
 };
 
 export interface Podcasts {
@@ -20,20 +19,28 @@ export interface Podcasts {
     status: Object;
     contents: {
       feed: {
-        entry: PodcastEntry[];
+        entry: Array<{
+          id: { attributes: { "im:id": string } };
+          "im:name": { label: string };
+          "im:artist": { label: string };
+          "im:image": Array<{
+            attributes: { height: string };
+            label: string;
+          }>;
+        }>;
       };
     };
   };
 }
 
 export interface usePodcastResponse {
-  podcasts: Podcasts | null;
+  podcasts: Podcast[] | null;
   loading: boolean;
 }
 
 function usePodcasts(url: string): usePodcastResponse {
   const [loading, setLoading] = useState(true);
-  const [podcasts, setPodcasts] = useState<Podcasts | null>(null);
+  const [podcasts, setPodcasts] = useState<Podcast[] | null>(null);
 
   useEffect(() => {
     const { data: cachedPodcasts, expirated } = getCache({
@@ -58,10 +65,10 @@ function usePodcasts(url: string): usePodcastResponse {
   return { podcasts, loading };
 }
 
-function parsePodcastList(rawPodcasts: string): Podcasts {
+function parsePodcastList(rawPodcasts: string): Podcast[] {
   let podcastsUnformatted = JSON.parse(rawPodcasts);
 
-  const podcastList = {
+  const rawPodcastList: Podcasts = {
     data: {
       contents: JSON.parse(podcastsUnformatted.data.contents),
       status: podcastsUnformatted.data.status,
@@ -69,7 +76,18 @@ function parsePodcastList(rawPodcasts: string): Podcasts {
     expirationDate: podcastsUnformatted.expirationDate,
   };
 
-  return podcastList;
+  const podcastEntries = rawPodcastList.data.contents.feed.entry.map(
+    (podcast) => {
+      return {
+        id: podcast.id.attributes["im:id"],
+        title: podcast["im:name"].label,
+        artist: podcast["im:artist"].label,
+        images: [...podcast["im:image"]],
+      };
+    }
+  );
+
+  return podcastEntries;
 }
 
 export { usePodcasts };
