d7893199a7e4064dc71d97e959f19c650318679d
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-11-08 16:14:29 +0100

    refactor routing in PodcastCard

--- src/app/components/PodcastCard/PodcastCard.tsx
+++ src/app/components/PodcastCard/PodcastCard.tsx
@@ -1,21 +1,15 @@
 import { Podcast } from "@/services/getPodcasts";
 import { StyledPodcastCard } from "./StyledPodcastCard";
 import Link from "next/link";
-import constants from "@/constants.json";
 
 function PodcastCard({ podcast }: { podcast: Podcast }) {
   return (
     <StyledPodcastCard>
       <article>
-        {/*
-          Since the real API it's down I will place a hardcoded image
-          
-       
-       <img alt="podcast-image" src={podcast.images[2].label}></img> */}
-        <Link
-          key={podcast.id}
-          href={constants.ROUTES["podcast-detail"] + `${podcast.id}`}
-        >
+        {/* previously, to make the route I used a constant. That's a good idea for ongoing projects, but since this one it's going to be
+         finished and never touched again, I decided to change it slightly to make it easier to read.
+         Old versiof of the routing -->   <Link key={podcast.id} href={constants.ROUTES["podcast-detail"] + `${podcast.id}`}> */}
+        <Link key={podcast.id} href={`/podcast/${podcast.id}`}>
           <img alt="podcast-image" src="/assets/imgs/Podcast.jpg" />
           <div className="box-separator">
             <h2>{podcast.title}</h2>
