20a21275d6cf02c0946d83b17595875124db435f
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-11-08 16:01:38 +0100

    refactor on Home and Dashboard tests

--- src/app/components/Dashboard/Dashboard.test.tsx
+++ src/app/components/Dashboard/Dashboard.test.tsx
@@ -1,46 +1,78 @@
 import "@testing-library/jest-dom";
-import { prettyDOM, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { prettyDOM, render, screen } from "@testing-library/react";
 
 import Dashboard from "./Dashboard";
 import PodcastOverview from "../PodcastOverview/PodcastOverview";
+import PodcastList from "../PodcastList/PodcastList";
+import { Podcast } from "@/services/getPodcasts";
+import { podcastsTemplate } from "@/assets";
 
 describe("DASHBOARD", () => {
-  test("dashboard should render a section owned by <PodcastList>", () => {
-    const view = render(
+  let mockPodcasts: Podcast[] = podcastsTemplate;
+
+  test("should render loading if the data still fetching", () => {
+    render(
+      <Dashboard loading={true}>
+        <PodcastOverview>
+          <PodcastList podcasts={[]}></PodcastList>
+        </PodcastOverview>
+      </Dashboard>
+    );
+
+    expect(screen.getByTestId("loader")).toBeInTheDocument();
+  });
+
+  test("should NOT render loading when data has been fetched", () => {
+    render(
       <Dashboard loading={false}>
-        <PodcastOverview podcasts={[]} />
+        <PodcastOverview>
+          <PodcastList podcasts={[]}></PodcastList>
+        </PodcastOverview>
+      </Dashboard>
+    );
+
+    expect(() => screen.getByTestId("loader")).toThrow();
+  });
+
+  test("should stay on the same route if it clicks the 'Podcaster'that has route '/' in it ", async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Dashboard loading={false}>
+        <PodcastOverview>
+          <PodcastList podcasts={mockPodcasts}></PodcastList>
+        </PodcastOverview>
+      </Dashboard>
+    );
+
+    // notice that the H1 that has "Podcaster" text gets treated as a "link" because of being wrapped inside a link
+    const home = screen.getByRole("link", { name: "Podcaster" });
+    expect(home).toBeInTheDocument();
+
+    //mockPodcasts[0].title === "Rock Music Podcast"
+    const podcast = screen.getByText(/Rock Music Podcast/i);
+    expect(podcast).toBeInTheDocument();
+
+    // user click on the 'home' button that redirects to '/' route (it's actually the very same page they are on it right now)
+    await user.click(home);
+
+    // user should be on the same route that has the list of podcasts
+    expect(podcast).toBeInTheDocument();
+  });
+
+  test("dashboard should render a section owned by <PodcastList>", () => {
+    render(
+      <Dashboard loading={false}>
+        <PodcastOverview>
+          <PodcastList podcasts={[]} />
+        </PodcastOverview>
       </Dashboard>
     );
     //console.log(prettyDOM(view.container));
 
-    const section = view.container.querySelector("section");
-    expect(section).toBeInTheDocument();
+    // this is owned by <PodcastList>
+    const podcastFilter = screen.getByPlaceholderText(/Filter podcasts.../i);
+    expect(podcastFilter).toBeInTheDocument();
   });
-
-  test("dashboard should render a loader when 'loading' = true", () => {
-    const view = render(
-      <Dashboard loading={true}>
-        <PodcastOverview podcasts={[]} />
-      </Dashboard>
-    );
-
-    const span = view.container.querySelector("span");
-    // console.log(prettyDOM(li as HTMLElement));
-    expect(span).toBeInTheDocument();
-  });
-
-  test("dashboard should NOT render a loader when 'loading' = false", () => {
-    const view = render(
-      <Dashboard loading={false}>
-        <PodcastOverview podcasts={[]} />
-      </Dashboard>
-    );
-
-    const span = view.container.querySelector("span");
-    // console.log(prettyDOM(li as HTMLElement));
-    expect(span).toBeNull();
-  });
-
-  // NOTE TO SELFIMPROVEMENT --> Another great test would be to know if Dashboard renders loading and at the same
-  // time it's rendering it's children.
 });
--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,11 +1,7 @@
 import "@testing-library/jest-dom";
-import { fireEvent, prettyDOM, render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 
 import Home from "./page";
-import Dashboard from "./components/Dashboard/Dashboard";
-import PodcastOverview from "./components/PodcastOverview/PodcastOverview";
-import PodcastList from "./components/PodcastList/PodcastList";
 import { podcastsTemplate } from "@/assets";
 import { Podcast } from "@/services/getPodcasts";
 
@@ -32,56 +28,6 @@ describe("HOME", () => {
 
     // console.log(prettyDOM(li as HTMLElement));
     expect(main).toBeInTheDocument();
-  });
-
-  test("should render loading if the data still fetching", () => {
-    render(
-      <Dashboard loading={true}>
-        <PodcastOverview>
-          <PodcastList podcasts={[]}></PodcastList>
-        </PodcastOverview>
-      </Dashboard>
-    );
-
-    expect(screen.getByTestId("loader")).toBeInTheDocument();
-  });
-
-  test("should NOT render loading when data has been fetched", () => {
-    render(
-      <Dashboard loading={false}>
-        <PodcastOverview>
-          <PodcastList podcasts={[]}></PodcastList>
-        </PodcastOverview>
-      </Dashboard>
-    );
-
-    expect(() => screen.getByTestId("loader")).toThrow();
-  });
-
-  test("should stay on the same route if it clicks the 'Podcaster'that has route '/' in it ", async () => {
-    const user = userEvent.setup();
-
-    render(
-      <Dashboard loading={false}>
-        <PodcastOverview>
-          <PodcastList podcasts={mockPodcasts}></PodcastList>
-        </PodcastOverview>
-      </Dashboard>
-    );
-
-    // notice that the H1 that has "Podcaster" text gets treated as a "link" because of being wrapped inside a link
-    const home = screen.getByRole("link", { name: "Podcaster" });
-    expect(home).toBeInTheDocument();
-
-    //mockPodcasts[0].title === "Rock Music Podcast"
-    const podcast = screen.getByText(/Rock Music Podcast/i);
-    expect(podcast).toBeInTheDocument();
-
-    // user click on the 'home' button that redirects to '/' route (it's actually the very same page they are on it right now)
-    await user.click(home);
-
-    // user should be on the same route that has the list of podcasts
-    expect(podcast).toBeInTheDocument();
   });
 
   /* 
